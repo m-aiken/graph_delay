5807514674b36cff9c81f06295bc71b1eaced239
m-aiken <mattaiken91@gmail.com>
2021-10-12 23:28:33 +0100

    clone of editor h from delay v1

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,11 +23,20 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void addControlLabel (juce::Slider &slider, juce::Label &label, const juce::String &labelText);
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     JuceDelayV2AudioProcessor& audioProcessor;
+    
+    juce::Slider timeSlider, feedbackSlider, wetSlider, drySlider;
+    juce::Label  timeLabel,  feedbackLabel,  wetLabel,  dryLabel;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+
+    Attachment timeSliderAttachment, feedbackSliderAttachment, wetSliderAttachment, drySliderAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (JuceDelayV2AudioProcessorEditor)
 };
